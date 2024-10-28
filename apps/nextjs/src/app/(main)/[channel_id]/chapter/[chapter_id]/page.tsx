75b839b33e3303d516d9ad023964300c4b715637
x-sss-x <jbportalsblr@gmail.com>
2024-10-28 19:04:05 +0530

    "Added search input and button to chapter page"

--- apps/nextjs/src/app/(main)/[channel_id]/chapter/[chapter_id]/page.tsx
+++ apps/nextjs/src/app/(main)/[channel_id]/chapter/[chapter_id]/page.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Search } from "lucide-react";
+
+import { Button } from "@bt/ui/button";
+import { Input } from "@bt/ui/input";
 
 export default function Page() {
-  return <div>Chapter</div>;
+  return (
+    <div>
+      <div className="flex gap-3">
+        <div className="relative flex w-full items-center">
+          <Search className="absolute ml-2.5 mr-2.5 size-5 text-muted-foreground" />
+          <Input placeholder="Search video..." className="h-10 ps-10" />
+        </div>
+        <Button size={"lg"}>Add New</Button>
+      </div>
+    </div>
+  );
 }
