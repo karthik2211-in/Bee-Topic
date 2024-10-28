51d7b8705c5f06237712ab657c7ae42e5141fb41
x-sss-x <jbportalsblr@gmail.com>
2024-10-28 19:07:42 +0530

    "Added PlayCircleIcon and empty state section to page component"

--- apps/nextjs/src/app/(main)/[channel_id]/chapter/[chapter_id]/page.tsx
+++ apps/nextjs/src/app/(main)/[channel_id]/chapter/[chapter_id]/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Search } from "lucide-react";
+import { PlayCircleIcon, Search } from "lucide-react";
 
 import { Button } from "@bt/ui/button";
 import { Input } from "@bt/ui/input";
@@ -14,6 +14,19 @@ export default function Page() {
         </div>
         <Button size={"lg"}>Add New</Button>
       </div>
+      <section
+        aria-label="Channels Empty"
+        className="flex flex-col items-center gap-3 py-40"
+      >
+        <PlayCircleIcon className="size-10" strokeWidth={1.25} />
+        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+          No Videos
+        </h4>
+        <p className="w-1/3 text-center text-sm text-muted-foreground">
+          Start adding videos by clicking on new video. Share your meaning full
+          knowledge with bees 🐝
+        </p>
+      </section>
     </div>
   );
 }
