152f08e30f36638fd2cde74cc5a940e2710b742b
x-sss-x <jbportalsblr@gmail.com>
2024-10-28 14:35:26 +0530

    "Update header styles in MainLayout"

--- apps/nextjs/src/app/(main)/layout.tsx
+++ apps/nextjs/src/app/(main)/layout.tsx
@@ -10,7 +10,7 @@ export default function MainLayout({
 }) {
   return (
     <main className="h-screen w-screen overflow-y-auto">
-      <header className="flex h-16 items-center justify-between border-b bg-secondary/20 px-6 backdrop-blur-sm">
+      <header className="sticky top-0 z-[9999] flex h-16 items-center justify-between border-b bg-secondary/25 px-6 backdrop-blur-xl">
         <h4 className="font-mono text-xl font-semibold">🐝BeeTopic</h4>
         <UserButton />
       </header>
