8cb2084ca142c68daf4f43d68c4a169345f373ee
x-sss-x <jbportalsblr@gmail.com>
2024-10-31 20:24:13 +0530

    "Updated study materials page to display a 'coming soon' message with a clock icon"

--- apps/nextjs/src/app/(main)/[channel_id]/chapter/[chapter_id]/(tabs)/study-materials/page.tsx
+++ apps/nextjs/src/app/(main)/[channel_id]/chapter/[chapter_id]/(tabs)/study-materials/page.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Clock10Icon } from "lucide-react";
 
 export default function Page() {
-  return <div>Study Materials</div>;
+  return (
+    <section
+      aria-label="Channels Empty"
+      className="flex flex-col items-center gap-3 py-40"
+    >
+      <Clock10Icon className="size-10" strokeWidth={1.25} />
+      <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+        Comming soon
+      </h4>
+      <p className="w-1/3 text-center text-sm text-muted-foreground">
+        We are trying hard to deliver this feature to you. So please be patience
+      </p>
+    </section>
+  );
 }
