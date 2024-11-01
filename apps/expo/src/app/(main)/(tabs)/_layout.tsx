ed06baa2712efbf109fb6acbc061c09fa9b58261
x-sss-x <jbportalsblr@gmail.com>
2024-11-01 15:24:25 +0530

    "Updated TabsLayout: changed borderTopWidth, height, and tint of BlurView, added useColorScheme hook"

--- apps/expo/src/app/(main)/(tabs)/_layout.tsx
+++ apps/expo/src/app/(main)/(tabs)/_layout.tsx
@@ -5,21 +5,24 @@ import "expo-blur";
 
 import { BlurView } from "expo-blur";
 
+import { useColorScheme } from "~/lib/useColorScheme";
+
 export default function TabsLayout() {
+  const { isDarkColorScheme } = useColorScheme();
   return (
     <Tabs
       screenOptions={{
         tabBarStyle: {
           position: "absolute", // Position the tab bar
-          borderTopWidth: 1, // Remove top border
+          borderTopWidth: 0.2, // Remove top border
           elevation: 0, // Remove shadow for Android
-          height: 64,
+          height: 54,
         },
         tabBarBackground() {
           return (
             <BlurView
               intensity={100} // Adjust intensity for the blur effect
-              tint="dark" // or 'dark'
+              tint={isDarkColorScheme ? "dark" : "light"} // or 'dark'
               style={{ flex: 1 }}
             />
           );
